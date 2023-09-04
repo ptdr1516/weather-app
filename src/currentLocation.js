72767ad44c59d81a1d8bd603c8fcd4975ea784f2
ptdr1516 <71786648+ptdr1516@users.noreply.github.com>
2023-09-04 12:41:12 +0530

    setting up lifecycle methods and function to get the current location

--- src/currentLocation.js
+++ src/currentLocation.js
@@ -34,6 +34,45 @@ const dateBuilder = (d) => {
     let year = d.getFullYear();
 
     return `${day}, ${date}, ${month}, ${year}`;
+};
+
+class Weather extends React.Component {
+    state = {
+        lat: undefined,
+        lon: undefined,
+        errorMessage: undefined,
+        temperatureC: undefined,
+        temperatureF: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        icon: "CLEAR_DAY",
+        sunrise: undefined,
+        sunset: undefined,
+        errorMsg: undefined,
+    };
+
+    componentDidMount() {
+        if (navigator.geolocation) {
+            this.getPosition() 
+               .then((position) => {
+                
+               })
+        }
+    }
+
+    componentWillUnmount() {
+        
+    }
+
+    getPosition = (options) => {
+        return new Promise(function (resolve, reject) {
+            navigator.geolocation.getCurrentPosition(resolve, reject, options);
+        });
+    };
+
+    get
 }
 
 export default Weather;
