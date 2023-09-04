a3ccc861af69c019a3805ae3470b21067c83a6b9
ptdr1516 <71786648+ptdr1516@users.noreply.github.com>
2023-09-04 12:23:07 +0530

    configure app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    
+    <React.Fragment>
+      <div className='container'>
+
+      </div>
+      <div className='footer-info'>
+        
+      </div>
+    </React.Fragment>
   );
 }
 
