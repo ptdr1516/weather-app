771ab2768ff8475e7d5d09bb9676c07ecc9768c7
ptdr1516 <71786648+ptdr1516@users.noreply.github.com>
2023-09-04 13:13:06 +0530

    render the data on web app

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    key : "{3ad21ce467762a1067b65bc811e7bd23}",
+    key : "3ad21ce467762a1067b65bc811e7bd23",
     base : "https://api.openweathermap.org/data/2.5/"
 }
--- src/currentLocation.js
+++ src/currentLocation.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Clock from 'react-live-clock';
 import ReactAnimatedWeather from 'react-animated-weather';
 import apiKeys from './apiKeys';
+import { defaults } from 'lodash';
 
 const dateBuilder = (d) => {
     let months = [
@@ -35,6 +36,12 @@ const dateBuilder = (d) => {
     let year = d.getFullYear();
 
     return `${day}, ${date}, ${month}, ${year}`;
+};
+
+const defaults = {
+    color: "white",
+    size: 112,
+    animate: true,
 };
 
 class Weather extends React.Component {
@@ -120,7 +127,45 @@ class Weather extends React.Component {
     };
 
     render() {
-        
+        if (this.state.temperatureC) {
+            return (
+                <React.Fragment>
+                    <div className='city'>
+                        <div className='title'>
+                            <h2>{this.state.city}</h2>
+                            <h3>{this.state.country}</h3>
+                        </div>
+                        <div className='mb-icon'>
+                            {" "}
+                            <ReactAnimatedWeather
+                                icon={this.state.icon}
+                                color={defaults.color}
+                                size={defaults.size}
+                                animate={defaults.animate}
+                            />
+                            <p>{this.state.main}</p>
+                        </div>
+                        <div className='date-time'>
+                            <div className='dmy'>
+                                <div id='txt'></div>
+                                <div className='current-time'>
+                                    <Clock format='HH:mm:ss' interval={1000} ticking={true} />
+                                </div>
+                                <div className='current-date'>
+                                    {dateBuilder(new Date())}
+                                </div>
+                            </div>
+                            <div className='temperature'>
+                                <p>
+                                    {this.state.temperatureC}°<span>C</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    </>
+                </React.Fragment>
+            )
+        }
     }
 }
 
