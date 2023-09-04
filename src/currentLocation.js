bb125d57bc37ef808e8a5d9603fa90c1eefb864e
ptdr1516 <71786648+ptdr1516@users.noreply.github.com>
2023-09-04 12:32:18 +0530

    creating date builder

---
+++ src/currentLocation.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Clock from 'react-live-clock';
+import ReactAnimatedWeather from 'react-animated-weather';
+
+const dateBuilder = (d) => {
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day}, ${date}, ${month}, ${year}`;
+}
+
+export default Weather;
